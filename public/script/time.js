3e208871c008f98d29545c98901fb6cc7085271f
Marcin Maciag <01159356@pw.edu.pl>
2022-04-19 20:27:23 +0200

    Login page changes

---
+++ public/script/time.js
@@ -0,0 +1,26 @@
+
+const clock = document.querySelector('.clock');
+
+const time = function() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    if(mm < 10) {
+        mm = '0' + mm;
+    }
+
+    if(hh < 10) {
+        hh = '0' + hh;
+    }
+
+    if(ss < 10) {
+        ss = '0' + ss;
+    }
+
+    let timerString = hh + ":" + mm + ":" + ss;
+    clock.textContent = timerString;
+}
+
+setInterval(time, 1000);
